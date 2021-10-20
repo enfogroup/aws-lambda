92f9fc8579dc210f89fbfb08e8c3e36ca14bee9b
Urban Pettersson <urbanpe88@gmail.com>
2021-10-20 07:36:35 +0200

    docs: add RecursivePartial

---
+++ src/models/common.ts
@@ -0,0 +1,9 @@
+/**
+ * Recursive partial which makes all keys of all objects optional
+ */
+export type RecursivePartial<T> = {
+  [P in keyof T]?:
+    T[P] extends (infer U)[] ? RecursivePartial<U>[] :
+    T[P] extends object ? RecursivePartial<T[P]> :
+    T[P];
+};
