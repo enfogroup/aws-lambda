f711afa2718f69d799a41f955788c6df384b6d1c
Urban Pettersson <urbanpe88@gmail.com>
2021-10-24 15:47:19 +0200

    docs: describe @models/handler

--- src/models/handler.ts
+++ src/models/handler.ts
@@ -2,8 +2,20 @@ import { STATUS } from '@models/http'
 
 export type Headers = Record<string, string>;
 
+/**
+ * Response from a Lambda integrated with API Gateway
+ */
 export interface HandlerResponse {
+  /**
+   * HTTP status code
+   */
   statusCode: STATUS;
+  /**
+   * Headers
+   */
   headers: Headers;
+  /**
+   * Optional body, stringified
+   */
   body?: string;
 }
