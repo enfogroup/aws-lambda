c4f42471dc1a5b906cd0a10fc623ecfcd982ad45
Urban Pettersson <urbanpe88@gmail.com>
2021-10-25 12:49:18 +0200

    chore: add more Response models

--- src/models/handler.ts
+++ src/models/handler.ts
@@ -2,10 +2,7 @@ import { HTTP_STATUS_CODE } from '@models/http'
 
 export type Headers = Record<string, string>;
 
-/**
- * Response from a Lambda integrated with API Gateway
- */
-export interface HandlerResponse {
+interface BaseResponse {
   /**
    * HTTP status code
    */
@@ -15,7 +12,32 @@ export interface HandlerResponse {
    */
   headers: Headers;
   /**
+   * If set to true the response will be treated as binary
+   */
+  isBase64Encoded?: boolean;
+}
+
+/**
+ * Response from a Lambda integrated with API Gateway
+ */
+export interface HandlerResponse extends BaseResponse {
+  /**
    * Optional body, stringified
    */
   body?: string;
 }
+
+/**
+ * Response complete with status code
+ */
+export interface ResponseWithStatusCode<T> extends BaseResponse {
+  /**
+   * Body of any response type. Will be stringified
+   */
+  body?: T
+}
+
+/**
+ * Response without status code. It should be supplied by the function to which this is being passed to
+ */
+export type ResponseWithoutStatusCode<T> = Omit<ResponseWithStatusCode<T>, 'statusCode'>
